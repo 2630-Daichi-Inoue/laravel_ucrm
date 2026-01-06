1f81ad77897ad7a65a6e0598860607c3974fe63e
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2026-01-06 20:23:36 +0900

    lesson 44

--- resources/js/Pages/Items/Index.vue
+++ resources/js/Pages/Items/Index.vue
@@ -42,10 +42,13 @@ defineProps({
                                         </thead>
                                         <tbody>
                                         <tr v-for="item in items" :key="item.id">
-                                            <td class="px-4 py-3">{{ item.id }}</td>
-                                            <td class="px-4 py-3">{{ item.name }}</td>
-                                            <td class="px-4 py-3">{{ item.price }}</td>
-                                            <td class="px-4 py-3">{{ item.is_selling }}</td>
+                                            <td class="px-4 py-3 border-b-2 border-gray-200">{{ item.id }}</td>
+                                            <td class="px-4 py-3 border-b-2 border-gray-200">{{ item.name }}</td>
+                                            <td class="px-4 py-3 border-b-2 border-gray-200">{{ item.price }}</td>
+                                            <td class="px-4 py-3 border-b-2 border-gray-200">
+                                                <span v-if="item.is_selling === 1">販売中</span>
+                                                <span v-if="item.is_selling === 0">停止中</span>
+                                            </td>
                                         </tr>
                                         </tbody>
                                     </table>
