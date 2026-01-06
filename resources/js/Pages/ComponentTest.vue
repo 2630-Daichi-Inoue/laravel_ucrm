49f9c75c450eee4d002879fd299949900cffac61
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2026-01-06 18:53:00 +0900

    lesson 34

--- resources/js/Pages/ComponentTest.vue
+++ resources/js/Pages/ComponentTest.vue
@@ -2,10 +2,15 @@
     import GuestLayout from '@/Layouts/GuestLayout.vue';
     import InputLabel from '@/Components/InputLabel.vue';
     import TextInput from '@/Components/TextInput.vue';
+
+    const emitTest = e => console.log(e);
+    
 </script>
 <template>
     <GuestLayout>
         <InputLabel value="件名">タイトル</InputLabel>
-        <TextInput modelValue="初期値が入る"></TextInput>
+        <TextInput
+        modelValue="初期値が入る"
+        @update.modelValue="emitTest"></TextInput>
     </GuestLayout>
 </template>
