8d9ef7ce898d01d20e6beb74e2ae994068bea6fa
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2026-01-21 07:33:49 +0900

    sync

--- resources/js/Pages/Purchases/Index.vue
+++ resources/js/Pages/Purchases/Index.vue
@@ -1,32 +1,30 @@
 <script setup>
+import { onMounted, ref } from 'vue'
 import FlashMessage from '@/Components/FlashMessage.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, Link } from '@inertiajs/vue3';
 import Pagination from '@/Components/Pagination.vue';
-import { ref } from 'vue';
 import { router } from '@inertiajs/vue3';
+import dayjs from 'dayjs';
 
-defineProps({
-    customers: Object,
+const props = defineProps({
+    orders: Object
 })
 
-const search = ref('')
-
-const searchCustomers = () => {
-    router.get(route('customers.index', { search: search.value }))
-}
-
+onMounted(() => {
+    console.log(props.orders.data)
+})
 </script>
 
 <template>
-    <Head title="顧客一覧" />
+    <Head title="購買履歴" />
 
     <AuthenticatedLayout>
         <template #header>
             <h2
                 class="text-xl font-semibold leading-tight text-gray-800"
             >
-                顧客一覧
+                購買履歴
             </h2>
         </template>
 
@@ -43,7 +41,6 @@ const searchCustomers = () => {
                                         <input type="text" name="search" v-model="search">
                                         <button @click="searchCustomers" class="bg-blue-300 text-white py-2 px-2">検索</button>
                                     </div>
-                                    <Link as="button" :href="route('customers.create')" class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">顧客登録</Link>
                                 </div>
                                 <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                                     <table class="table-auto w-full text-left whitespace-no-wrap">
@@ -51,26 +48,30 @@ const searchCustomers = () => {
                                         <tr>
                                             <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">ID</th>
                                             <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">氏名</th>
-                                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">メアド</th>
-                                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">電話番号</th>
+                                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">合計金額</th>
+                                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">ステータス</th>
+                                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">購入日</th>
                                             <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
                                         </tr>
                                         </thead>
                                         <tbody>
-                                        <tr v-for="customer in customers.data" :key="customer.id">
+                                        <tr v-for="order in props.orders.data" :key="order.id">
                                             <td class="px-4 py-3 border-b-2 border-gray-200">
-                                                {{ customer.id }}
+                                                <Link class="text-blue-400" :href="route('purchases.show', { purchase: order.id })">
+                                                    {{ order.id }}
+                                                </Link>
                                             </td>
-                                            <td class="px-4 py-3 border-b-2 border-gray-200">{{ customer.name }}</td>
-                                            <td class="px-4 py-3 border-b-2 border-gray-200">{{ customer.email }}</td>
-                                            <td class="px-4 py-3 border-b-2 border-gray-200">{{ customer.tel }}</td>
+                                            <td class="px-4 py-3 border-b-2 border-gray-200">{{ order.customer_name }}</td>
+                                            <td class="px-4 py-3 border-b-2 border-gray-200">{{ order.total }}</td>
+                                            <td class="px-4 py-3 border-b-2 border-gray-200">{{ order.status }}</td>
+                                            <td class="px-4 py-3 border-b-2 border-gray-200">{{ dayjs(order.created_at).format('YYYY-MM-DD HH:mm:ss') }}</td>
                                         </tr>
                                         </tbody>
                                     </table>
                                 </div>
 
                             </div>
-                            <Pagination class="mt-6" :links="customers.links"></Pagination>
+                            <Pagination class="mt-6" :links="props.orders.links"></Pagination>
                         </section>
 
                     </div>
