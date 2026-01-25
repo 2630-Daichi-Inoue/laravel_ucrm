ba758ecd725fce5fd9b12b121a780d325f9ce21c
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2026-01-26 07:02:16 +0900

    decile

--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -4,6 +4,7 @@ import { Head } from '@inertiajs/vue3';
 import { reactive, onMounted } from 'vue';
 import { getToday } from '@/common';
 import Chart from '@/Components/Chart.vue';
+import ResultTable from '@/Components/ResultTable.vue';
 
 const form = reactive({
     startDate: null,
@@ -33,6 +34,7 @@ const getData = async () => {
             data.data = res.data.data
             data.labels = res.data.labels
             data.totals = res.data.totals
+            data.type = res.data.type
             console.log(res.data)
         })
     } catch(e) {
@@ -47,20 +49,39 @@ const getData = async () => {
 
     <AuthenticatedLayout>
         <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800"
-            >
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 データ分析
             </h2>
         </template>
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
-                >
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         <form @submit.prevent="getData">
+                            分析方法<br>
+
+                            <input type="radio" v-model="form.type" value="perDay" checked>
+                            <span class="mr-2">
+                                日別
+                            </span>
+
+                            <input type="radio" v-model="form.type" value="perMonth">
+                            <span class="mr-2">
+                                月別
+                            </span>
+
+                            <input type="radio" v-model="form.type" value="perYear">
+                            <span class="mr-2">
+                                年別
+                            </span>
+
+                            <input type="radio" v-model="form.type" value="decile">
+                            <span class="mr-2">
+                                デシル分析
+                            </span>
+                            <br>
+
                             From: <input type="date" name="startDate" v-model="form.startDate">
                             To: <input type="date" name="endDate" v-model="form.endDate"><br>
                             <button class="mt-4 flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">分析する</button>
@@ -68,23 +89,7 @@ const getData = async () => {
 
                         <div v-show="data.data">
                             <Chart :data="data" />
-                        </div>
-
-                        <div v-show="data.data" class="lg:w-2/3 w-full mx-auto overflow-auto">
-                            <table class="table-auto w-full text-left whitespace-no-wrap">
-                                <thead>
-                                <tr>
-                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">年月日</th>
-                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">金額</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr v-for="item in data.data" :key="item.date">
-                                    <td class="px-4 py-3 border-b-2 border-gray-200">{{ item.date }}</td>
-                                    <td class="px-4 py-3 border-b-2 border-gray-200">{{ item.total }}</td>
-                                </tr>
-                                </tbody>
-                            </table>
+                            <ResultTable :data="data" />
                         </div>
 
                     </div>
